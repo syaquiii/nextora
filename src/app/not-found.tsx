a7dbf21d2890e6cff5a034b528b39b6d340499dd
syaquiii <msyauqioqi@gmail.com>
2025-05-26 16:49:14 +0700

    feat : add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-200 to-gray-300 text-gray-800">
+      <div className="text-center p-8 rounded-lg bg-white shadow-lg animate-fadeIn">
+        <h1 className="text-4xl font-bold">
+          🚧 Page still Under Construction 🚧
+        </h1>
+        <p className="mt-4 text-lg">
+          Sabar yaa, website nya masih dalam pengembangan
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
